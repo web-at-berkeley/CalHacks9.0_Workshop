8d0212e4dae9ffd4441e899f8e4150c040b27df6
Alina Wang <55339055+aliner-wang@users.noreply.github.com>
2022-10-14 02:44:05 -0700

    adding starter and final code:

---
+++ final/main.js
@@ -0,0 +1,30 @@
+function submission_confirmation(e) {
+    // This e.preventDefault() line is optional. I'm keeping it here for debugging and demo purposes 
+    // so that Form submission does not reset the console. App will still work without this line 
+    // you just can't see the values being sent or anything printed in console when form submitted
+    e.preventDefault();
+
+    // Get the value from each input object in index.html
+    var name = document.getElementById("name").value;
+    var location = document.getElementById("location").value;
+    var incident = document.getElementById("incident").value;
+
+    // print msg sent to Spiderman in console 
+    console.log('Hi Spiderman. ')
+    console.log(name + ' needs your help at location: ' + location);
+    console.log(incident);
+
+    // To actually send a text msg, make sure to set up TWILLIO and uncomment line 18 (next line)
+    send_text_msg()
+
+    // Alert your user that the text msg to Spiderman is sent
+    alert("Your request has been sent to Spiderman!")
+}
+
+
+// Function for sending actual test msgs. 
+// Built using Twilio api on free trial
+function send_text_msg() {
+    console.log("sending msg....")
+
+}
---
+++ starter/main.js
@@ -0,0 +1,4 @@
+function submission_confirmation(e) {
+    e.preventDefault();
+    // TODO: Fill in the rest 
+}
